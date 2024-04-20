b4fbe1f62864ff60f894ce7575f1f1e12a40c0c4
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-04-20 14:59:28 -0500

    add database configuration

--- src/main/java/com/develliotesco/carms/CarMsApplication.java
+++ src/main/java/com/develliotesco/carms/CarMsApplication.java
@@ -1,5 +1,7 @@
 package com.develliotesco.carms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CarMsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CarMsApplication.class, args);
+		final Logger log = LoggerFactory.getLogger(CarMsApplication.class);
+		log.info("Bienvenido a demo de microservicios - car ms");
 	}
 
 }
